887c0ece2da817acfc1f1e77d5c2c161ddd42079
PeterZhizhin <piter.zh@gmail.com>
2020-07-02 13:50:22 +0300

    Removed unnecessary files, project started.

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,78 @@
-#include <functional>
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <fmt/core.h> // for print
+#include <fmt/ostream.h> // for print
+#include <fstream>
 #include <iostream>
+#include <spdlog/spdlog.h> // for info
+#include <string_view>
+#include <tuple>
 
-#include <spdlog/spdlog.h>
 
+ABSL_FLAG(std::string, file, "", "Output file path (stdout by default)");
+ABSL_FLAG(uint32_t, width, 256, "Output image width");
+ABSL_FLAG(uint32_t, height, 256, "Output image height");
 
-#include <docopt/docopt.h>
+static constexpr int colors = 256;
 
-#include <iostream>
+using color_range_t = std::array<double, 2>;
+static constexpr color_range_t red_range = { 0.0, 0.99999 };
+static constexpr color_range_t green_range = { 0.0, 0.99999 };
+static constexpr color_range_t blue_range = { 0.25, 0.25 };
 
-static constexpr auto USAGE =
-  R"(Naval Fate.
-
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
-
-int main(int argc, const char **argv)
+template<typename FloatType>
+constexpr int float_color_to_int(const FloatType c)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+  int ic = static_cast<int>(colors * c);
+  return ic;
+}
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
+template<typename FloatType>
+constexpr std::tuple<int, int, int> float_color_to_int(const FloatType r, const FloatType g, const FloatType b)
+{
+  int ir = float_color_to_int(r);
+  int ig = float_color_to_int(g);
+  int ib = float_color_to_int(b);
+  return { ir, ig, ib };
+}
+
+void print_simple_image(const uint32_t image_width, const uint32_t image_height, std::ostream *output_stream)
+{
+  fmt::print(*output_stream, "P3\n{} {}\n{}\n", image_width, image_height, colors - 1);
+  for (uint32_t height = 0; height != image_height; ++height) {
+    spdlog::info("Progress: {}/{} lines.", height, image_height);
+    for (uint32_t width = 0; width != image_width; ++width) {
+      auto rt = static_cast<double>(width) / (image_width - 1);
+      auto gt = static_cast<double>(height) / (image_height - 1);
+      auto bt = 0.0;
+
+      auto r = std::lerp(red_range[0], red_range[1], rt);
+      auto g = std::lerp(green_range[0], green_range[1], gt);
+      auto b = std::lerp(blue_range[0], blue_range[1], bt);
+
+      auto [ir, ig, ib] = float_color_to_int(r, g, b);
+      fmt::print(*output_stream, "{} {} {}\n", ir, ig, ib);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  absl::ParseCommandLine(argc, argv);
+  const auto output_file_path = absl::GetFlag(FLAGS_file);
+  std::ostream *output_stream = &std::cout;
+  std::ofstream output_file;
+  if (!output_file_path.empty()) {
+    spdlog::info("Outputting to {}", output_file_path);
+    output_file.open(output_file_path);
+    output_stream = &output_file;
   }
 
+  const auto width = absl::GetFlag(FLAGS_width);
+  const auto height = absl::GetFlag(FLAGS_height);
 
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
-
-  fmt::print("Hello, from {}\n", "{fmt}");
+  print_simple_image(width, height, output_stream);
 }
